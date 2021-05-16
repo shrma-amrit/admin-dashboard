efe8fa1fa4989ca1fd350a32c2459f0f726955a7
Amrit Sharma <amrit@Amrits-MacBook-Air.local>
2021-05-16 22:19:43 +0530

    Updates

---
+++ precache-manifest.6c401be0fab6693349621b8f878003af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73490874d39888d4744e312bd1d58c3e",
+    "url": "/admin-dashboard/index.html"
+  },
+  {
+    "revision": "50093b37396b806fb809",
+    "url": "/admin-dashboard/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "758f80de3a967bfaf364",
+    "url": "/admin-dashboard/static/css/main.6605bfe1.chunk.css"
+  },
+  {
+    "revision": "50093b37396b806fb809",
+    "url": "/admin-dashboard/static/js/2.7a284098.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/admin-dashboard/static/js/2.7a284098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "758f80de3a967bfaf364",
+    "url": "/admin-dashboard/static/js/main.95bdfdd7.chunk.js"
+  },
+  {
+    "revision": "bf1a6a320ae045ff10e3",
+    "url": "/admin-dashboard/static/js/runtime-main.49b439ae.js"
+  }
+]);
